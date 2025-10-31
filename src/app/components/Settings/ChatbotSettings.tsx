0eb5361ab709dba0c58769f29d91c525134e14b1
バンドウイ茶 <59186289+bondICha@users.noreply.github.com>
2025-10-31 18:09:00 +0900

    Add beta remaks in setting

--- src/app/components/Settings/ChatbotSettings.tsx
+++ src/app/components/Settings/ChatbotSettings.tsx
@@ -616,7 +616,7 @@ const ChatbotSettings: FC<Props> = ({ userConfig, updateConfigValue }) => {
                             <path d="M4 3a2 2 0 100 4h12a2 2 0 100-4H4z"/>
                             <path fillRule="evenodd" d="M3 8h14v7a2 2 0 01-2 2H5a2 2 0 01-2-2V8zm5 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" clipRule="evenodd"/>
                           </svg>
-                          <p className="text-sm font-semibold text-blue-900 dark:text-blue-100">{t('Image Generation Settings')}</p>
+                          <p className="text-sm font-semibold text-blue-900 dark:text-blue-100">{t('Image Generation Settings')} (Beta)</p>
                         </div>
 
                         <div className={formRowClass}>
